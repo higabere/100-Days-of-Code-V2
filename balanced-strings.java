fcc1bed7b0c7b6967e00568d0a8c2e967131ec4c
higabere <95122378+higabere@users.noreply.github.com>
2022-09-09 16:02:52 -0500

    Create balanced-strings.java

---
+++ balanced-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+       int countR=0,countL=0, k= 0, num=0 ;
+        String[] newS = s.split("");
+        for (String a: newS){
+            k = a.equals("R")? ++countR: ++countL;
+            if (countR == countL)
+                ++num;
+        }
+        return num;
+    }
+}
