3fa1503fab7180183207eeefa6758c1f0b2f0641
higabere <95122378+higabere@users.noreply.github.com>
2022-09-13 11:23:51 -0500

    Create Day04-jewels-and-stones.java

---
+++ Day04-jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        char [] charJewel = jewels.toCharArray();
+        
+        for(char a : charJewel){
+			for(int i=0; i<stones.length(); i++){
+				if(stones.charAt(i) == a)
+					count++;
+			}
+        }
+        return count;
+    }
+}
